d56e83326706d3a04683aac13468d1b16d621876
Fabio <fabio.missagia@gmail.com>
2023-02-16 16:58:31 +0100

    Bug fixes

--- controllers/annuncio.js
+++ controllers/annuncio.js
@@ -1,31 +1,36 @@
 const Annuncio = require('../models/annuncio');
+const Utente = require('../models/utente');
 const Locale = require('../models/locale');
 
 const publish = async (req, res) => {
-	// create a new user
-	console.log(arredato);
+	// create a new ad
 	const nuovoAnnuncio = new Annuncio({
 		foto: req.body.foto,
 		numero_bagni: req.body.numero_bagni,
 		numero_locali: req.body.numero_locali,
-		locali: req.body.locali,
+		locali: null,
 		prezzo: req.body.prezzo,
 		classe_energetica: req.body.classe_energetica,
 		indirizzo: req.body.indirizzo,
-		arredato: req.body.arredato,
+		arredato: null,
 		vetrina: {
-			data_inizio: {type: Date},
-			data_fine: {type: Date}, 
+			data_inizio: new Date(),
+			data_fine: null
     	}
 	});
 
-	// save the user
-	nuovoUtente.save((err) => {
+	//salviamo l'annuncio nel db
+	nuovoAnnuncio.save((err) => {
 		if (err) {
 			return res.status(500).json({ Error: "Internal server error: " + err });
 		}
-		return res.status(201).json({ success: true, message: 'User created successfully.' });
+		return res.status(201).json({ success: true, message: 'Ad created successfully.' });
 	});
+
+	//lo inseriamo nella lista degli annunci salvati dell'utente
+	const utente = await Utente.findById(req.body.userId);
+	utente.annunci_pubblicati.push(nuovoAnnuncio._id);
+	await utente.save();
 }
 
 module.exports = {
--- controllers/utente.js
+++ controllers/utente.js
@@ -39,6 +39,7 @@ const login = async (req, res) => {
 		success: true,
 		message: 'Enjoy your token!',
 		token: token,
+		id: user.id,
 		nome: user.nome,
 		cognome: user.cognome
 	});
