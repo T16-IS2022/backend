f9e839ee6086af2b0e1910ae38066a70327f0e40
Rafiu Hossain <rafiuhoss888@gmail.com>
2023-06-03 22:58:35 +0200

    swagger now works after deployment.

--- swagger.js
+++ swagger.js
@@ -4,7 +4,18 @@ const swaggerDocument = require('./swagger.json');
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
 function swaggerConfig(app) {
-    app.use('/api-docs', express.static('node_modules/swagger-ui-dist/', { index: false }), swaggerUi.serve, swaggerUi.setup(swaggerDocument, { customCssUrl: CSS_URL }));
+    app.use(
+        '/api-docs', 
+        express.static(
+            'node_modules/swagger-ui-dist/', 
+            { index: false }
+        ), 
+        swaggerUi.serve, 
+        swaggerUi.setup(
+            swaggerDocument, 
+            { customCssUrl: CSS_URL }
+        )
+    );
 }
 
 module.exports = swaggerConfig;
