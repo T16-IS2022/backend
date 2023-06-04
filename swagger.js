48208bc3f308822b32c3e27fba5183b1c97eb887
Rafiu Hossain <rafiuhoss888@gmail.com>
2023-06-04 10:26:04 +0200

    vercel fix

--- swagger.js
+++ swagger.js
@@ -4,6 +4,10 @@ const swaggerDocument = require('./swagger.json');
 const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 
 function swaggerConfig(app) {
+    if(process.env.NODE_ENV != 'deploy') {
+        swaggerDocument.host = 'localhost:3000';
+        swaggerDocument.schemes = ['http'];
+    }
     app.use(
         '/api-docs', 
         express.static(
