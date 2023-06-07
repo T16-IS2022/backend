e8a813969f96ece39fa84712a08457e806d1d67f
Fabio <fabio.missagia@gmail.com>
2023-06-07 18:41:17 +0200

    Refactor token checker

--- middleware/tokenChecker.js
+++ middleware/tokenChecker.js
@@ -1,26 +1,28 @@
-const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const jwt = require('jsonwebtoken');
+
 
 const tokenChecker = async (req, res, next) => {
-	// header or url parameters or post parameters
-	var token = req.body.token || req.query.token || req.headers['x-access-token'];
-	if (!token) 
-		return res.status(400).json({ 
-			success: false, 
-			message: 'No token provided.' 
-		});
-	// decode token, verifies secret and checks expiration
-	jwt.verify(token, process.env.SUPER_SECRET, function (err, decoded) {
-		if (err) 
-			return res.status(401).json({ 
-				success: false, 
-				message: 'Token not valid' 
-			});
-		else {
-			// if everything is good, save in req object for use in other routes
-			req.loggedUser = decoded.data;
-			next();
-		}
-	});
+    // header or url parameters or post parameters
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+    if (!token)
+        return res.status(400).json({
+            code: 400,
+            message: 'No token provided.'
+        });
+    // decode token, verifies secret and checks expiration
+    jwt.verify(token, process.env.SUPER_SECRET, function (err, decoded) {
+        if (err)
+            return res.status(401).json({
+                code: 401,
+                message: 'Token not valid'
+            });
+        else {
+            // if everything is good, save in req object for use in other routes
+            req.loggedUser = decoded.data;
+            next();
+        }
+    });
 };
+
 
 module.exports = tokenChecker
